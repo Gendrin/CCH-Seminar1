7f6e868b65478a00045bdb68b4a97f794f672e95
GendrinVL <GendrinVL@gmail.com>
2022-08-05 22:49:28 +1000

    Задачи на максимумы

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -6,5 +6,25 @@ else
 {
     seminar1 sem1 = new seminar1(Int32.Parse(snumber1));
 }
-
-
+Console.WriteLine("Задача №2, проверка на максимум - два числа.");
+Console.WriteLine("Введите число №1  к задаче №2, проверка на максимум.");
+string? x1 = Console.ReadLine();
+Console.WriteLine("Введите число №2  к задаче №2, проверка на максимум.");
+string? y1 = Console.ReadLine();
+if ((x1 == null)||(y1 == null)) Console.WriteLine("Введено значение null!");
+else
+{
+    seminar1 sem1 = new seminar1(Int32.Parse(x1),Int32.Parse(y1));
+}
+Console.WriteLine("Задача №4, проверка на максимум - три числа.");
+Console.WriteLine("Введите число №1  к задаче №4, проверка на максимум.");
+x1 = Console.ReadLine();
+Console.WriteLine("Введите число №2  к задаче №4, проверка на максимум.");
+y1 = Console.ReadLine();
+Console.WriteLine("Введите число №3  к задаче №4, проверка на максимум.");
+string? z1 = Console.ReadLine();
+if ((x1 == null)||(y1 == null)||(z1 == null)) Console.WriteLine("Введено значение null!");
+else
+{
+    seminar1 sem1 = new seminar1(Int32.Parse(x1),Int32.Parse(y1),Int32.Parse(z1));
+}
--- Seminar1/seminar1.cs
+++ Seminar1/seminar1.cs
@@ -2,7 +2,7 @@ namespace Seminar1
 {
     public class seminar1
     {
-         private int number1;
+      private int number1;
       private bool numchet;
       public int Number
         {
@@ -13,12 +13,26 @@ namespace Seminar1
         {
             get {return numchet;}
         }
-    public seminar1 (int n)
+    public seminar1 (int n) // Конструктор с определением четности
     {
         number1=n; if (n%2==0) 
                         {numchet=true;Console.WriteLine($"Введенное число: {n} четное");}
                         else
                             {numchet=false;Console.WriteLine($"Введенное число: {n} нечетное");}
     }
+    public seminar1 (int x, int y) // Конструктор с определением максимального числа из двух чисел
+    {
+        if (x>y) Console.WriteLine($"Максимальное число из двух чисел: {x}");
+            else Console.WriteLine($"Максимальное число из двух чисел: {y}");
     }
+    public seminar1 (int x, int y, int z) // Конструктор с определением максимального числа из трех чисел
+    {
+        int max;
+        if (x>y) max=x;
+            else if (y>z) max=y;
+                else max=z;
+            Console.WriteLine($"Максимальное число из трех чисел: {max}");
+    }
+    }
+
 }
