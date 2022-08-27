c24c38d794018a4cc68205639dddd100ab40b413
GendrinVL <GendrinVL@gmail.com>
2022-08-28 06:28:55 +1000

    Add new tasks for seminar N7

---
+++ Seminar7/DZ7.cs
@@ -0,0 +1,149 @@
+namespace DZ7
+{
+    public class dz
+    {
+         public enum quantSigns
+            {
+                oneDigit = 1,
+                twoDigit,
+                threeDigit,
+                fourDigit,
+                fiveDigit
+            }
+
+        public int[,] GetRandomArrayInteger(int longArrayRows, int longArrColumns, quantSigns quantArraySigns, bool plusAndMinus)
+        {
+            int[,] resultArray = new int[longArrayRows, longArrColumns];
+            Random randomInteger = new Random();
+            int randomStart = 1 * (int)(Math.Pow(10, (int)quantArraySigns - 1));
+            int randomEnd = (1 * (int)Math.Pow(10, (int)quantArraySigns));
+            if (plusAndMinus) randomStart = 0 - randomEnd;
+            for (int i = 0; i < resultArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < resultArray.GetLength(1); j++)
+                {
+                    resultArray[i,j] = randomInteger.Next(randomStart, randomEnd);
+                }
+            }
+            return resultArray;
+        }
+
+        public int[,] GetRandomArrayInteger(int longArrayRows, int longArrColumns, int rendomStart, int rendomEnd)
+        {
+            int[,] resultArray = new int[longArrayRows, longArrColumns];
+            Random randomInteger = new Random();
+            for (int i = 0; i < resultArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < resultArray.GetLength(1); j++)
+                {
+                    resultArray[i, j] = randomInteger.Next(rendomStart, rendomEnd);
+                }
+
+            }
+            return resultArray;
+        }
+
+        public double[,] GetRandomArrayDouble(int longArrayRows, int longArrayColumns, int randomMaxNumber, bool plusAndMinus)
+        {
+            double[,] resultArray = new double[longArrayRows, longArrayColumns];
+            Random randomDouble = new Random(); //Рендом для вещественного числа
+            Random randomZero = new Random(); //Рендом знака для вещественного числа
+            int randomStartZero = -1;
+            int randomFinZezo = 2;
+            int znak = 1;
+            for (int i = 0; i < resultArray.GetLength(0); i++)
+
+            {
+                for (int j = 0; j < resultArray.GetLength(1); j++)
+                {
+                    if (plusAndMinus)
+                    {
+                        do
+                        {
+                            znak = randomZero.Next(randomStartZero, randomFinZezo);
+                        }
+                        while (znak == 0);
+                    }
+                    resultArray[i, j] = Math.Round(randomDouble.NextDouble() * randomMaxNumber * znak, 2);
+                }
+
+            }
+            return resultArray;
+        }
+
+        public double[,] GetRandomArrayDouble(int longArrayRows,int longArrayColumns, quantSigns quantArraySigns, bool plusAndminus)
+        {
+            double[,] resultArray = new double[longArrayRows, longArrayColumns];
+            Random randomDouble = new Random(); //Рендом для вещественного числа
+            Random randomZero = new Random(); //Рендом знака для вещественного числа
+            double randomEnd = 1 * Math.Pow(10, (int)quantArraySigns) - 1;
+            int randomStartZero = -1;
+            int randomFinishZezo = 2;
+            int znak = 1;
+            for (int i = 0; i < resultArray.GetLength(0); i++)
+
+            {
+                for (int j = 0; j < resultArray.GetLength(1); j++)
+                {
+                    if (plusAndminus)
+                    {
+                        do
+                        {
+                            znak = randomZero.Next(randomStartZero, randomFinishZezo);
+                        }
+                        while (znak == 0);
+                    }
+                    resultArray[i,j] = Math.Round(randomDouble.NextDouble() * randomEnd * znak, 2);
+                }
+            }
+            return resultArray;
+        }
+
+        public void PrintArray<T>(T[,] parray)
+        {
+            for (int i = 0; i < parray.GetLength(0); i++)
+            {
+                for (int j = 0; j < parray.GetLength(1); j++)
+                {
+                    Console.Write($"{parray[i, j],6} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void PrintArray<T>(T[] parray)
+        {
+            for (int i = 0; i < parray.Length; i++)
+            {
+                if (i == parray.Length - 1) Console.Write($"{parray[i]}");
+                else Console.Write($"{parray[i]}; ");
+            }
+            Console.Write(".");
+        }
+        // Task50
+        public double? FindElementArray (int rowElement, int columnElement, double[,] arrayForFind)
+        {
+            if (rowElement < arrayForFind.GetLength(0) && columnElement < arrayForFind.GetLength(1))
+            {
+                return arrayForFind[rowElement, columnElement];
+            }
+            else return null;
+        }
+
+        public double[] ArithmeticAverageColumns (int[,] arrayForArithmetic)
+        {
+            double[] result = new double[arrayForArithmetic.GetLength(1)];
+            
+            for (int j = 0; j < arrayForArithmetic.GetLength(1); j++)
+            {
+                double sumColumn = 0;
+                for (int i = 0; i < arrayForArithmetic.GetLength(0); i++)
+                {
+                    sumColumn += arrayForArithmetic[i, j];
+                }
+                result[j] = Math.Round(sumColumn/arrayForArithmetic.GetLength(0),1);
+            }
+            return result;
+        }
+    }
+}
---
+++ Seminar7/Program.cs
@@ -0,0 +1,93 @@
+﻿using DZ7;
+
+dz dz7 = new dz();
+//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+string[] inputString;
+bool resultinLongArrRows;
+bool resultinLongArrColumns;
+Console.WriteLine("Please enter quantity rows & columnw array through whitespase!");
+inputString = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+resultinLongArrRows = int.TryParse(inputString[0], out int longArrayRows);
+resultinLongArrColumns = int.TryParse(inputString[1], out int longArrayColumns);
+if(resultinLongArrRows&&resultinLongArrColumns)
+{
+    if(longArrayRows>0&&longArrayColumns>0)
+    {
+        Console.WriteLine($"m={longArrayRows},n={longArrayColumns}.");
+        double[,] arrayDoubleNumbers = new double[longArrayRows, longArrayColumns];
+        arrayDoubleNumbers=dz7.GetRandomArrayDouble(longArrayRows, longArrayColumns, dz.quantSigns.oneDigit, true);
+        dz7.PrintArray<double>(arrayDoubleNumbers);
+    }
+    else Console.WriteLine("No correct Input number!Operation breaked!");
+}
+else Console.WriteLine("No correct Input number! Operation breaked!");
+Console.WriteLine();
+
+//Задача 50.Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+//формируем массив случайных чисел 
+Console.WriteLine("Please enter quantity rows & columnw array through whitespase for task50!");
+inputString = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+resultinLongArrRows = int.TryParse(inputString[0], out int longArrayRowsTask50);
+resultinLongArrColumns = int.TryParse(inputString[1], out int longArrayColumnsTask50);
+//double[,] arrayDoubleTask50;
+if (resultinLongArrRows && resultinLongArrColumns)
+{
+    if (longArrayRowsTask50 > 0 && longArrayColumnsTask50 > 0)
+    {
+        Console.WriteLine("Random array.");
+        double[,] arrayDoubleTask50 = new double[longArrayRowsTask50, longArrayColumnsTask50];
+        arrayDoubleTask50 = dz7.GetRandomArrayDouble(longArrayRowsTask50, longArrayColumnsTask50, dz.quantSigns.oneDigit, true);
+        dz7.PrintArray<double>(arrayDoubleTask50);
+
+        Console.WriteLine("Please choose element up prints arrays! Insert number row and column.");
+        bool resultinElementArrRow;
+        bool resultinElementArrColumn;
+        inputString = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        resultinElementArrRow = int.TryParse(inputString[0], out int findElementRow);
+        resultinElementArrColumn = int.TryParse(inputString[1], out int findElementColumn);
+        Console.WriteLine($"Find element in array row={findElementRow}, column={findElementColumn}.");
+
+        if (resultinElementArrRow && resultinElementArrColumn)
+        {
+            if (findElementRow >= 0 && findElementColumn >= 0)
+            {
+                Console.Write($"{findElementRow}, {findElementColumn} -> ");
+                if (dz7.FindElementArray(findElementRow, findElementColumn, arrayDoubleTask50) != null)
+                {
+                    Console.Write($"{dz7.FindElementArray(findElementRow, findElementColumn, arrayDoubleTask50)}");
+                }
+                else Console.Write($"такого элемента в массиве нет");
+
+            }
+            else Console.WriteLine("No correct Input number!Operation breaked!");
+        }
+        else Console.WriteLine("No correct Input number! Operation breaked!");
+
+    }
+    else Console.WriteLine("No correct Input number!Operation breaked!");
+}
+else Console.WriteLine("No correct Input number! Operation breaked!");
+
+Console.WriteLine();
+//Задача 52.Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.WriteLine("Please enter quantity rows & columnw array through whitespase for task 52!");
+inputString = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+resultinLongArrRows = int.TryParse(inputString[0], out int longArrayRowsTask52);
+resultinLongArrColumns = int.TryParse(inputString[1], out int longArrayColumnsTask52);
+if (resultinLongArrRows && resultinLongArrColumns)
+{
+    if (longArrayRowsTask52 > 0 && longArrayColumnsTask52 > 0)
+    {
+        Console.WriteLine("Random array.");
+        int[,] arrayIntegerTask52 = new int[longArrayRowsTask52, longArrayColumnsTask52];
+        arrayIntegerTask52 = dz7.GetRandomArrayInteger(longArrayRowsTask52, longArrayColumnsTask52, dz.quantSigns.oneDigit, true);
+        dz7.PrintArray<int>(arrayIntegerTask52);
+        Console.Write("Среднее арифметическое каждого столбца: ");
+        dz7.PrintArray<double>(dz7.ArithmeticAverageColumns(arrayIntegerTask52));
+    }
+    else Console.WriteLine("No correct Input number!Operation breaked!");
+}
+else Console.WriteLine("No correct Input number! Operation breaked!");
+Console.WriteLine();
